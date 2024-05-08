1efa780a872aab34db07ead19a686cab56fb2640
tom <tom@the-cloud>
2024-05-08 16:59:58 +0000

    bumped taa and m4 deps.

--- project.clj
+++ project.clj
@@ -1,20 +1,20 @@
-(def version "4.2.18")
+(def version "4.2.19")
 (def capsule-name "m4top")
 (def capsule-jar (str  capsule-name "-" version ".jar"))
 
-(defproject m4top "4.2.18-SNAPSHOT"
+(defproject m4top "4.2.19-SNAPSHOT"
   :description "Top level artifact for M4 app."
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.11.1"]
-                 [marathon  "4.2.15-SNAPSHOT"]
+                 [marathon  "4.2.16-SNAPSHOT"]
                  ;;external libs
                  [joinr/nightclub "0.0.4-SNAPSHOT"
                   :exclusions [commons-codec commons-io]]
                  [eigenhombre/splasher "0.0.2"] ;;splash screen lib
                  ;;taa
-                 [taa "0.0.19-SNAPSHOT"]
+                 [taa "0.0.20-SNAPSHOT"]
                  ]
   :plugins [[reifyhealth/lein-git-down "0.4.1"]]
   :middleware [lein-git-down.plugin/inject-properties]
@@ -22,7 +22,7 @@
   :git-down {marathon {:coordinates fsdonks/m4}
              taa      {:coordinates fsdonks/taa}}
   :profiles {:dev {:source-paths [;;"../spork/src" "../nightclub/src"
-                                  "../proc/src"
+                                  ;;"../proc/src"
                                   ;;"../marathon-schemas/src"
                                   ]}
              :uberjar {:aot  [marathon.main]
