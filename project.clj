cb15c31a6b7db67bfaa9907c62592a73f18c6b81
tom <tom@that-place>
2024-11-18 17:36:50 -0600

    Working on aot fren peer

--- project.clj
+++ project.clj
@@ -2,31 +2,36 @@
 (def capsule-name "m4top")
 (def capsule-jar (str  capsule-name "-" version ".jar"))
 
-(defproject m4top "4.2.22-SNAPSHOT"
+(defproject m4top "4.2.23-SNAPSHOT"
   :description "Top level artifact for M4 app."
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.11.1"]
-                 [marathon  "4.2.19-SNAPSHOT"]
+                 [marathon  "4.2.20-SNAPSHOT"]
                  ;;external libs
                  [joinr/nightclub "0.0.4-SNAPSHOT"
                   :exclusions [commons-codec commons-io]]
                  [eigenhombre/splasher "0.0.2"] ;;splash screen lib
                  ;;taa
-                 [taa "0.0.23-SNAPSHOT"]
+                 [taa "1df93aaaebb75639a30b599ea3d5d3bbf04242ca" #_"0.0.23-SNAPSHOT"]
                  ]
   :plugins [[reifyhealth/lein-git-down "0.4.1"]]
   :middleware [lein-git-down.plugin/inject-properties]
   :repositories [["public-github" {:url "git://github.com"}]]
   :git-down {marathon {:coordinates fsdonks/m4}
              taa      {:coordinates fsdonks/taa}
-             demand_builder  {:coordinates  fsdonks/demand_builder}}
+             demand_builder  {:coordinates  fsdonks/demand_builder}
+             proc     {:coordinates  fsdonks/proc}
+             marathon-schemas {:coordinates fsdonks/marathon-schemas}
+             hazeldemo {:coordinates joinr/hazeldemo}}
   :profiles {:dev {:source-paths [;;"../spork/src" "../nightclub/src"
                                   ;;"../proc/src"
                                   ;;"../marathon-schemas/src"
+                                  "../taa/src"
+                                  "../taa/test/"
                                   ]}
-             :uberjar  {:aot [marathon.main marathon.core marathon.analysis.random]
+             :uberjar  {:aot [marathon.main marathon.core marathon.analysis.random chazel.core hazeldemo.utils]
                         :main  marathon.main
                         :jvm-opts ^:replace ["-Xmx1000m" "-XX:NewSize=200m" "-server"]
                         :plugins [[lein-capsule "0.2.1"]]
--- test/m4top/core_test.clj
+++ test/m4top/core_test.clj
@@ -1,7 +1,17 @@
 (ns m4top.core-test
   (:require [clojure.test :refer :all]
             [marathon.ces.testing]
-            [marathon.ui]))
+            [marathon.ui]
+            #_
+            [chazel.core]
+            [taa.capacity-test]
+            )
+  #_
+  (:import [chazel.core Ctask]))
 
+(println [:testing :m4top.core-test])
+#_#_
 (run-tests 'marathon.ces.testing)
 (require 'taa.test-hook)
+
+(run-tests 'taa.capacity-test)
