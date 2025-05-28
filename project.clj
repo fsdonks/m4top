544393c21f665c274187aded3296467329d7a21c
tom <tom@the-cloud>
2025-05-28 22:15:46 +0000

    Pruning dependencies
    Down to ~195mb from 250.
    We can get to 185 if we rip out the leiningen
    and related jank from nightclub.
    
    This is somewhat academic, but still it'd be nice

--- project.clj
+++ project.clj
@@ -1,36 +1,36 @@
-(def version "4.2.23")
-(def capsule-name "m4top")
-(def capsule-jar (str  capsule-name "-" version ".jar"))
-
 (defproject m4top "4.2.23-SNAPSHOT"
   :description "Top level artifact for M4 app."
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :exclusions [org.clojure/clojurescript
+               com.google.javascript/closure-compiler-unshaded]
   :dependencies [[org.clojure/clojure "1.12.0"]
                  [marathon  "4.2.20-SNAPSHOT"
                   :exclusions [com.cnuernber/ham-fisted com.taoensso/nippy org.clojure/data.csv
                                commons-codec
                                org.clojure/core.rrb-vector]]
+                 [org.clojure/data.csv "1.1.0"]
                  [org.clojure/core.rrb-vector "0.2.0"]
                  ;;external libs
                  [joinr/nightclub "0.0.5-SNAPSHOT"
-                  :exclusions [commons-codec commons-io org.clojure/core.async hiccup org.clojure/java.classpath
-                               org.clojure/data.codec]]
+                  :exclusions [commons-codec commons-io org.clojure/core.async hiccup
+                               #_org.clojure/java.classpath
+                               #_org.clojure/data.codec]]
                  [eigenhombre/splasher "0.0.2"] ;;splash screen lib
                  ;;taa
                  [taa "20f16dafb96ef69f47ff891d9532d09a0e09a689" #_"0.0.23-SNAPSHOT"
                   :exclusions [commons-codec]]
                  [com.taoensso/nippy "2.15.3" :exclusions [com.taoensso/encore com.taoensso/truss]]
                  [babashka/process "0.5.22"]
-                 [taapost "0.1.0-SNAPSHOT"
-                  :exclusions [jfree/jfreechart jfree/jfreechart com.taoensso/nippy commons-codec]]
+                 [taapost "0.1.1-SNAPSHOT"
+                  :exclusions [jfree/jfreechart jfree/jfreechart com.taoensso/nippy
+                               commons-codec]]
                  ;;these were tacked on due to taapost, we can look at cleaning this up
                  [commons-codec "1.15"]
                  [com.taoensso/timbre "6.6.1"]
                  [com.taoensso/encore  "3.128.0"]
-                 [com.taoensso/truss "1.12.0"]
-                 ]
+                 [com.taoensso/truss "1.12.0"]]
   :plugins [[reifyhealth/lein-git-down "0.4.1"]]
   :middleware [lein-git-down.plugin/inject-properties]
   :repositories [["public-github" {:url "git://github.com"}]]
@@ -51,7 +51,7 @@
                                   ]
                    :aot [chazel.core hazeldemo.utils]}
              :uberjar  {:aot [marathon.main marathon.core marathon.analysis.random chazel.core hazeldemo.utils
-                              taapost.shave taapost.nlist]
+                              tech.v3.dataset]
                         :main  marathon.main
                         :jvm-opts ^:replace ["-Xmx1000m" "-XX:NewSize=200m" "-server"]
                         }}
